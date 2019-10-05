8afbd374925b1a04f73f24088019f7192843c461
Tristan Batchler <batchlertristan@gmail.com>
2019-10-05 19:30:30 +1000

    test collaboration

--- MoonlightMUD/Program.cs
+++ MoonlightMUD/Program.cs
@@ -7,6 +7,7 @@ namespace MoonlightMUD
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing collaboration!");
         }
     }
 }
