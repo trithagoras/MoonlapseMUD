b267fa20e5d1c746de4738529ab4b122b1e8f667
tristanbatchler <batchlertristan@gmail.com>
2019-12-22 10:24:52 +1000

    Write logs to file

--- server/log.py
+++ server/log.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Log:
     def __init__(self):
         self.state = {
@@ -9,3 +12,7 @@ class Log:
         # Log times as seconds since 1-Jan-1970 (Unix time)
         self.latest = {timestamp: message}
         self.state['log'].update(self.latest)
+
+        datestr: str = time.strftime('%Y-%m-%d', time.gmtime())
+        with open(f"{datestr}.log", 'a') as f:
+            print(f"{time.strftime('%R', time.gmtime(timestamp))}: {message}", file=f)
