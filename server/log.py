e256189ada7800639d26df5ef25b68d953357166
tristanbatchler <batchlertristan@gmail.com>
2019-12-22 10:34:44 +1000

    Make server/logs if it doesn't exist

--- server/log.py
+++ server/log.py
@@ -15,6 +15,9 @@ class Log:
         self.state['log'].update(self.latest)
 
         logdir: str = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logs')
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
+
         datestr: str = time.strftime('%Y-%m-%d', time.gmtime())
         logfile: str = os.path.join(logdir, f"{datestr}.log")
         with open(logfile, 'a') as f:
