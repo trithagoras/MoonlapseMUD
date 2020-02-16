4c1a33569c57caa2b421380850ed205c56ccde76
tristanbatchler <batchlertristan@gmail.com>
2020-02-16 17:07:28 +1000

    MM-8 Prepare for merge with MM-8

--- client/view.py
+++ client/view.py
@@ -198,22 +198,83 @@ class GameView(View):
             self.title(self.win3, "[3] Log")
 
         # Window 1 content
-        for index in range(0, len(self.game.game_data['p'])):
-            player = self.game.game_data['p'][index]
-
-            if player is not None:
-                self.win1.addch(player['pos']['y'] + 1, player['pos']['x'], player['c'])
-
-        for wall in self.game.walls:
-            self.win1.addch(wall[1] + 1, wall[0], '█')
+        self.draw_map()
 
         # Window 2 content
         if self.win2_focus == Window2Focus.SKILLS:
-            self.status_win()
+            self.draw_status_win()
         elif self.win2_focus == Window2Focus.HELP:
-            self.help_win()
+            self.draw_help_win()
 
         # Window 3 content
+        self.draw_log()
+
+    def draw_map(self):
+        sight_radius = 10
+
+        player = self.game.game_data['p'][self.game.player_id]
+
+        for row in range(-sight_radius, sight_radius + 1):
+            for col in range(-sight_radius, sight_radius + 1):
+
+                pos = (player['pos']['y'] + row, player['pos']['x'] + col)
+
+                if self.coordinate_exists(pos[0], pos[1]):
+                    # drawing walls
+                    if [pos[1], pos[0]] in self.game.walls:
+                        self.win1.addch(11 + row, 26 + col * 2, '█')
+                    else:
+                        self.win1.addch(11 + row, 26 + col * 2, '·')
+
+                    # drawing other players
+                    for index in range(0, len(self.game.game_data['p'])):
+                        other_player = self.game.game_data['p'][index]
+                        if other_player is not None and other_player is not player:
+                            if (other_player['pos']['y'], other_player['pos']['x']) == pos:
+                                self.win1.addch(11 + row, 26 + col * 2, '☺')
+
+        # drawing player to centre of screen
+        self.win1.addch(11, 26, '☻')
+
+    def draw_help_win(self):
+        self.win2.addstr(2, 2, "Navigating the 3 game panels can be done with the")
+        self.win2.addstr(3, 2, "[1], [2], [3] keys.")
+        self.win2.addstr(4, 2, "Controls for each focused panel is shown above.")
+
+        self.win2.addstr(6, 2, "The second panel [2] can be changed to show")
+        self.win2.addstr(7, 2, "other screens which can be focused and")
+        self.win2.addstr(8, 2, "interacted with. The keys to do so are")
+        self.win2.addstr(9, 2, "found in the help section below:")
+
+        self.win2.addstr(11, 2, "[?] Help")
+        self.win2.addstr(12, 2, "[K] Skills")
+        self.win2.addstr(13, 2, "[I] Inventory")
+        self.win2.addstr(14, 2, "[P] Spellbook")
+        self.win2.addstr(15, 2, "[G] Guild / Social")
+        self.win2.addstr(16, 2, "[J] Journal")
+
+    def draw_status_win(self):
+        self.win2.addstr(1, 2, "coreyb65, Guardian of Forgotten Moor")
+        self.win2.addstr(3, 2, f"Level 15 {self.progress_bar(7, 10)} (7/10 skill levels to 16)")
+
+        self.win2.addstr(5, 2, f"Vitality      31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(6, 2, f"Strength      10/10 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(7, 2, f"Agility       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(8, 2, f"Dexterity     31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(9, 2, f"Astrology     31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(10, 2, f"Intelligence  31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+
+        self.win2.addstr(12, 2, f"Woodcutting   31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(13, 2, f"Crafting      31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(14, 2, f"Mining        31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(15, 2, f"Smithing      31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(16, 2, f"Fishing       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(17, 2, f"Cooking       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(18, 2, f"Alchemy       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(19, 2, f"Enchanting    31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+        self.win2.addstr(20, 2, f"??????????    31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+
+    def draw_log(self):
         self.win3.hline(self.win3_height - 3, 1, ncurses.ACS_HLINE, self.win3_width - 2)
 
         # Fill the log
@@ -236,43 +297,8 @@ class GameView(View):
         # Add chat prompt
         self.win3.addstr(15, 2, "Say: ")
 
-    def help_win(self):
-        self.win2.addstr(2, 2, "Navigating the 3 game panels can be done with the")
-        self.win2.addstr(3, 2, "[1], [2], [3] keys.")
-        self.win2.addstr(4, 2, "Controls for each focused panel is shown above.")
-
-        self.win2.addstr(6, 2, "The second panel [2] can be changed to show")
-        self.win2.addstr(7, 2, "other screens which can be focused and")
-        self.win2.addstr(8, 2, "interacted with. The keys to do so are")
-        self.win2.addstr(9, 2, "found in the help section below:")
-
-        self.win2.addstr(11, 2, "[?] Help")
-        self.win2.addstr(12, 2, "[K] Skills")
-        self.win2.addstr(13, 2, "[I] Inventory")
-        self.win2.addstr(14, 2, "[P] Spellbook")
-        self.win2.addstr(15, 2, "[G] Guild / Social")
-        self.win2.addstr(16, 2, "[J] Journal")
-
-    def status_win(self):
-        self.win2.addstr(1, 2, "coreyb65, Guardian of Forgotten Moor")
-        self.win2.addstr(3, 2, f"Level 15 {self.progress_bar(7, 10)} (7/10 skill levels to 16)")
-
-        self.win2.addstr(5, 2, f"Vitality      31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(6, 2, f"Strength      10/10 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(7, 2, f"Agility       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(8, 2, f"Dexterity     31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(9, 2, f"Astrology     31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(10, 2, f"Intelligence  31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-
-        self.win2.addstr(12, 2, f"Woodcutting   31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(13, 2, f"Crafting      31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(14, 2, f"Mining        31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(15, 2, f"Smithing      31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(16, 2, f"Fishing       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(17, 2, f"Cooking       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(18, 2, f"Alchemy       31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(19, 2, f"Enchanting    31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
-        self.win2.addstr(20, 2, f"??????????    31/31 {self.progress_bar(3, 10)} (3,000/10,000)")
+    def coordinate_exists(self, y: int, x: int) -> bool:
+        return 0 <= y < self.game.size[0] and 0 <= x < self.game.size[1]
 
     @staticmethod
     def progress_bar(value: float, max_value: float) -> str:
