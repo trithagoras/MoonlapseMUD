71101dd79fb9923b94a3a3b7f1f796a6d29c83e8
tristanbatchler <batchlertristan@gmail.com>
2020-05-05 01:24:16 +1000

    MM-19: Make some db queries sycnchronous

--- server/database.py
+++ server/database.py
@@ -1,5 +1,7 @@
 import json
 from twisted.enterprise import adbapi
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import Deferred
 import datetime
 from typing import *
 import traceback
@@ -41,10 +43,11 @@ class Database:
 
         print(f"Done.")
 
-    def register_player(self, username, password):
+    @inlineCallbacks
+    def register_player(self, username: str, password: str) -> None:
         print(f"Attempting to register player to database: {username}:{password}...", end='')
         now: str = str(datetime.datetime.utcnow())
-        self.dbpool.runQuery(f"""=
+        yield self.dbpool.runOperation(f"""=
             INSERT INTO users (username, password)
             VALUES ('{username}', '{password}');
 
@@ -58,12 +61,12 @@ class Database:
             WHERE u.username = '{username}'
             AND e.lastupdated = '{now}';
         """)
+        print("Done.")
 
-        print("Success!")
-
+    @inlineCallbacks
     def user_exists(self, username: str) -> bool:
         print(f"Checking if user {username} exists...", end='')
-        self.curs.execute(f"""
+        result: bool = yield self.dbpool.runQuery(f"""
             SELECT CASE 
                 WHEN EXISTS (
                     SELECT NULL
@@ -73,13 +76,13 @@ class Database:
                   ELSE FALSE
             END;
         """)
-        result = self.curs.fetchone()[0]
-        print(f"Result: {result}")
+        print("Done")
         return result
 
+    @inlineCallbacks
     def password_correct(self, username: str, password: str) -> bool:
-        print(f"Checking if credentials {username}:{password} are correct...", end='')
-        self.curs.execute(f"""
+        print(f"Checking if credentials {username}:{password} are correct...")
+        result: bool = yield self.dbpool.runQuery(f"""
             SELECT CASE 
                 WHEN EXISTS (
                     SELECT NULL
@@ -90,15 +93,14 @@ class Database:
                   ELSE FALSE
             END;
         """)
-        result = self.curs.fetchone()[0]
-        print(f"Result: {result}")
+        print("Done.")
         return result
 
-    def update_player_pos(self, player: models.Player, x: int, y: int) -> None:
-        # print(f"Updating player position ({player.get_username()})...", end='')
-        self.curs.execute(f"""
+    def update_player_pos(self, player: models.Player, y: int, x: int) -> Deferred:
+        print(f"Updating player position ({player.get_username()})...")
+        return self.dbpool.runOperation(f"""
             UPDATE entities
-            SET position = '{x}, {y}'
+            SET position = '{y}, {x}'
             WHERE id IN (
                 SELECT p.entityid
                 FROM players AS p
@@ -106,12 +108,10 @@ class Database:
                 ON p.userid = u.id and u.username = '{player.get_username()}' 
             )
         """)
-        self.conn.commit()
-        print(f"Done! New position: ({x}, {y}).")
 
-    def get_player_pos(self, player: models.Player) -> Tuple[int, int]:
+    def get_player_pos(self, player: models.Player) -> Deferred:
         print(f"Getting player position ({player.get_username()})...", end='')
-        self.curs.execute(f"""
+        return self.dbpool.runQuery(f"""
             SELECT position[0], position[1]
             FROM entities
             where id IN (
@@ -121,10 +121,3 @@ class Database:
                 ON p.userid = u.id AND u.username = '{player.get_username()}'
             )
         """)
-        x, y = self.curs.fetchone()
-        if (x, y) != (None, None):
-            result = (int(x), int(y))
-        else:
-            result = (x, y)
-        print(f"Result: {result}")
-        return result
